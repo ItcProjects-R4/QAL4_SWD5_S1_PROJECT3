e3ddc0e05c0aadcf2d872a4eebdcc5de5a6f6a7e
Maryam <maryam.khaled0510@gmail.com>
2026-06-06 01:42:35 +0300

    Complete doctor module backend

---
+++ src/SehhaTech.Core/DTOs/Doctor/UpdateAppointmentStatusRequest.cs
@@ -0,0 +1,9 @@
+﻿using SehhaTech.Core.Models;
+
+namespace SehhaTech.Core.DTOs.Doctor
+{
+    public class UpdateAppointmentStatusRequest
+    {
+        public AppointmentStatus Status { get; set; }
+    }
+}
